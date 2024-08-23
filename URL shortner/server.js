8f7dd9c29ddec84fdc860961eea40bd7ac1071eb
sadsaxninja <78135349+sadsaxninja@users.noreply.github.com>
2024-08-23 13:11:50 +0200

    Add files via upload

---
+++ URL shortner/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Define Routes
+app.use('/api/url', require('./routes/url'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
